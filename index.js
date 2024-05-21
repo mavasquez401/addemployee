ea9c112141d6415862e8eaa9d52889ec2908cffa
mavasquez401 <mavasquez401@gmail.com>
2024-05-20 22:24:07 -0400

    added login func

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 
-const mysql = require('mysql2');
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const mysql = require('mysql2')
+const express = require('express')
+const bodyParser = require('body-parser')
+require('dotenv').config()
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-});
+})
 
 connection.connect(err => {
-    if (err) throw err;
-    console.log('Connected to MySQL Server!');
+    if (err) throw err
+    console.log('Connected to MySQL Server!')
 });
 
 // Middleware to parse request bodies
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Serve the HTML file
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html')
 });
-
-
-
-
 
 function storeData(event) {
   event.preventDefault(); // Prevent the default form submission
@@ -42,8 +38,10 @@ function storeData(event) {
   let jobTitle = document.getElementById('jobTitle').value
   let hireDate = document.getElementById('hireDate').value
   let endDate = document.getElementById('endDate').value
-  let statusUpdate = document.getElementById('status');
-  
+  let statusUpdate = document.getElementById('status')
+  let Username = document.getElementById('Username').value
+  let Password = document.getElementById('Password').value
+
   const employee = {
     FirstName: fname,
     LastName: lname,
@@ -51,31 +49,21 @@ function storeData(event) {
     JobTitle: jobTitle,
     StartDate: hireDate,
     EndDate: endDate,
-    Salary: salary
+    Salary: salary,
+    Username: Username,
+    Password: Password
+
   };
   
   if (employee) {
-    console.log(employee);
-    statusUpdate.innerHTML = '<p style="color:green">Saved</p>';
+    console.log(employee)
+    statusUpdate.innerHTML = '<p style="color:green">Saved</p>'
   }
 }
 
-// app.get('/', function (req, res) {
-//   // res.send('Hello Geeks!')  
-//   // instead of hello geeks, res.send index.html
-//   const { FirstName, LastName, Department, JobTitle, StartDate, EndDate, Salary } = employee;
-//   connection.query(`INSERT INTO ${process.env.DB_TABLE_NAME} SET ?`, { FirstName, LastName, Department, JobTitle, StartDate, EndDate, Salary }, err => {
-//     if (err) throw err;
-//     console.log('1 record inserted');
-//     res.send('Employee added successfully');
-    
-//     // res.redirect('/listemployee'); // Redirect to list employees after adding
-//   });
-// })
-
 // Endpoint to handle form submissions
 app.post('/addemployee', (req, res) => {
-    const { fname, lname, dept, salary, jobTitle, hireDate, endDate } = req.body;
+    const { fname, lname, dept, salary, jobTitle, hireDate, endDate, Username, Password } = req.body;
     const employee = {
         FirstName: fname,
         LastName: lname,
@@ -83,36 +71,34 @@ app.post('/addemployee', (req, res) => {
         JobTitle: jobTitle,
         StartDate: hireDate,
         EndDate: endDate,
-        Salary: salary
-    };
+        Salary: salary,
+        Username: Username,
+        Password: Password
+    }
 
     connection.query('INSERT INTO employees SET ?', employee, (err) => {
         if (err) {
-            console.error('Error inserting data: ', err);
-            return res.status(500).send('Error inserting data');
+            console.error('Error inserting data: ', err)
+            return res.status(500).send('Error inserting data')
         }
-        console.log('1 record inserted');
-        res.send('Employee added successfully');
-    });
-  });
-  // app.post('/addemployee', (req, res) => {
-    //   const { FirstName, LastName, Department, JobTitle, StartDate, EndDate, Salary } = employee;
-    //   connection.query('INSERT INTO employees SET ?', { FirstName, LastName, Department, JobTitle, StartDate, EndDate, Salary }, err => {
-      //       if (err) throw err;
-      //       console.log('1 record inserted');
-      //       res.send('Employee added successfully');
+        console.log('1 record inserted')
+        res.send('Employee added successfully')
+    })
+  })
       
-      //       // res.redirect('/listemployee'); // Redirect to list employees after adding
-      //   });
-      // });
-      
-      
+  app.post('/login',(req, res) => {
+    const { Username, Password } = req.body
+    connection.query('SELECT * FROM employees WHERE Username = ? AND Password = ?', [Username, Password], (err, results) =>{
+      if (err) throw err
+      if (results.length > 0) {
+        res.json({ success: true})
+      } else {
+        res.json({ success: false})
+      }
+    })
+  })
+
       app.listen(PORT, () => {
-        console.log(`Node.js server running at http://localhost:${PORT}`);
-      });
-      
-      
-//       app.listen(PORT, () => {
-//         console.log(`Node.js server running at http://localhost:${PORT}`);
-//   console.log(`add user to database at http://localhost:${PORT}/addemployee`);
-// })
+        console.log(`Node.js server running at http://localhost:${PORT}`)
+      })
+
